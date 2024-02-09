5de3620c3233b3b04a699507d519ca6063994ecd
Mohammed Ali <alimoh22062000@gmail.com>
2024-02-09 11:34:14 +0100

    [Functionality], Cite search enabled

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,21 @@
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
-      <input v-model="searchQuery" type="text" placeholder="Search for a City/State"
+      <input @input="getSearchResults" v-model="searchQuery" type="text" placeholder="Search for a City/State"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-blue-900 focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+
+        <ul v-if="mapBoxSearchResults !== null" class="absolute bg-blue-900 text-white w-full shadow-md py-2 px-1 top-[66px]">
+          <li v-for="item in mapBoxSearchResults" :key="item.id" class="py-2 cursor-pointer">
+            {{ item.place_name }}
+          </li>
+          <!-- Since our search results come with an ID each, I can easily make the 'binding key' the IDs to maintain the uniqueness. -->
+        </ul>
     </div>
   </main>
 </template>
+<!-- This searching of location is done using the lazy search approach. -->
+
+<!-- So "@input" means the moment User starts typing. -->
 
 
 <script setup>
@@ -30,16 +40,23 @@ const mapBoxSearchResults = ref(null);
 // Now, after 300ms i.e 3secs, the request to the API should be made, and should be stored into the variable 'queryTimeOut'
 const getSearchResults = () => {
 
+    clearTimeout(queryTimeOut.value);
+  // I need to clear the timeOut each time the function is ran.
+
+  //  The reason you need to place the clearTimeout() function before the setTimeout() call is because you want to ensure that if the getSearchResults() function is called multiple times within the timeout period, the previous timeout is canceled before setting a new one. This prevents the function from being executed multiple times if the user continues to type within the specified timeout period.
+
   queryTimeOut.value = setTimeout(async () => {
 
     // Trying to check if the 'searchQuery' isn't empty before I make my API fetch coz of course I'm not gonna make any request if the input field is empty.
     if (searchQuery.value !== "") {
 
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapBoxAPIKey}`);
+      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapBoxAPIKey}&types=place`);
       // The endpoint is gonna return a promise, that's why I used async-await.
 
       // Storing the main/final data we need in a variable.
       mapBoxSearchResults.value = result.data.features;
+
+      console.log(mapBoxSearchResults.value);
 
       return;
       // I needed to return....because even if our condition is true, it'll execute the task right, and also proceed to execute all other tasks that come after. And we don't want that.
@@ -50,6 +67,8 @@ const getSearchResults = () => {
     mapBoxSearchResults.value = null;
 
   }, 300)
+
+
 
 }
 
