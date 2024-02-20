a82c0d1bdda6295f7f81c7b15861b6280cce838e
Mohammed Ali <alimoh22062000@gmail.com>
2024-02-20 17:02:08 +0100

    [Functionality], handling async city view

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+import axios from 'axios';
+
+import { useRoute } from 'vue-router';
+// This is to get access to the route ( url parameter ) coz I need some data from it.
+
+const route = useRoute();
+// Initializing the route.
+
+const getWeatherData = async () => {
+
+    try {
+        const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=f1dbcc4cff472ccd01050ed08b93b12f&units=imperial`);
+        // The value for lat and lng were gotten from the route parameters
+
+
+        // calculating current date & time
+        // Because of the difference Eastern Timezone and the Pacific Timezone.
+        const localOffset = new Date().getTimezoneOffset() * 60000;
+        const utc = weatherData.data.current.dt * 1000 + localOffset;
+        weatherData.data.currentTime =
+            utc + 1000 * weatherData.data.timezone_offset;
+
+        // calculating hourly weather offset
+        weatherData.data.hourly.forEach((hour) => {
+            const utc = hour.dt * 1000 + localOffset;
+            hour.currentTime =
+                utc + 1000 * weatherData.data.timezone_offset;
+        });
+
+        return weatherData.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Invoking our function
+const weatherData = await getWeatherData();
+// So I can use this "weatherData" in the markup and have access to a lot of data.
+
+console.log(weatherData);
+</script>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-
+<AsyncCityView />
     </div>
 </template>
 
 <script setup>
-
+import AsyncCityView from '../components/AsyncCityView.vue';
 </script>
 
